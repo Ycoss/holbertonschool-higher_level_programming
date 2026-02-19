0fbdd4f1ff97bdd92a2b6b1eb5cac805df1c315d
Ycoss <ylandacoss@gmail.com>
2026-02-18 18:53:31 -0600

    Add sandbox setup script

---
+++ SQL_more_queries/emacs/setup_sandbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p /root/emacs
+echo 'C-x C-f' > /root/emacs/opening
+echo 'C-x C-s' > /root/emacs/saving
+echo 'C-k' > /root/emacs/cutting
+echo 'C-y' > /root/emacs/pasting
+echo 'C-s' > /root/emacs/searching
+echo 'C-x u' > /root/emacs/undoing
+echo 'C-x C-c' > /root/emacs/quitting
